668fe68c2ada69f6670c4e8d5cd7aa2326bf3880
Juan Pablo Muñoz <juanpablo@lm.net.co>
2018-09-26 16:50:39 -0500

    Renderizar en ReactJs, Crear componentes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Mis importaciones
+import { todos } from './todos.json';
+
 class App extends Component {
+  
+  constructor(){
+    super();
+    this.state = {
+      todos
+    }
+  }
+
   render() {
+
+    const objTodos = this.state.todos.map((todo, i) => {
+      return(
+        <div className="col-md-4">
+          <div className="card">
+          <div className="card-header">
+            <h3>{todo.title}</h3>
+          </div>
+          <div className="card-body">
+            <p>{todo.description}</p>
+          </div>
+        </div>
+        </div>
+      );
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
+          <nav className="navbar navbar-dark bg-dark">
+            <a href="" className="text-white">
+              Tasks
+            </a>
+          </nav>
+
+          <div className="container">
+            <div className="row mt-4">
+            { objTodos }
+            </div>
+          </div>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenido to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
